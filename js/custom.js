cfb79620afd147c676e9dc8e27ad92ba2e86daef
amfever505 <amfeve505@gmail.com>
2022-02-25 23:19:38 +0900

    spinner

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,17 @@
-// console.log(true);
-// $(window).scroll(function () {
-//   var a = $(window).scrollTop();
-//   console.log(a);
-// });
+window.addEventListener('DOMContentLoaded', function () {
+  $('.thumnail-box').imagesLoaded(function () {
+    // 画像を読み込んだ後に実行したい処理
+    console.log('loaded');
+    $('#spinner').hide();
+  });
+  $('.card').on('click', () => {
+    console.log('click');
+    $('#spinner').show();
+
+    $('.thumnail-box').imagesLoaded(function () {
+      // 画像を読み込んだ後に実行したい処理
+      console.log('loaded');
+      $('#spinner').hide();
+    });
+  });
+});
