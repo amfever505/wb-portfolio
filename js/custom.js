ff8160a14e45e73c4656a0de4e9f5d5639c85268
amfever505 <amfeve505@gmail.com>
2023-04-08 17:20:29 +0900

    remove spinner, add work

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,13 @@
 window.addEventListener('DOMContentLoaded', function () {
   $('.thumnail-box').imagesLoaded(function () {
     // 画像を読み込んだ後に実行したい処理
-    console.log('loaded');
     $('#spinner').hide();
   });
   $('.card').on('click', () => {
-    console.log('click');
     $('#spinner').show();
 
     $('.thumnail-box').imagesLoaded(function () {
       // 画像を読み込んだ後に実行したい処理
-      console.log('loaded');
       $('#spinner').hide();
     });
   });
